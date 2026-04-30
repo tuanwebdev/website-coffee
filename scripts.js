44a9ab4bb534d2bfe62739f9ba70d0fdd81b000a
tuanwebdev <webltrinh3@gmail.com>
2026-04-30 23:24:20 +0700

    thêm gallery

--- scripts.js
+++ scripts.js
@@ -1,10 +1,42 @@
+// Menu
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
-  // Toggle mobile menu visibility
-  document.body.classList.toggle("show-mobile-menu");
+  document.body.classList.toggle("is-mobile-menu-open");
 });
 
-// Close menu when the close button is clicked
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.remove("is-mobile-menu-open");
+});
+
+
+// Swiper
+const swiper = new Swiper('.testimonials__wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  pagination: {
+    el: '.testimonials__pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: '.testimonials__button--next',
+    prevEl: '.testimonials__button--prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
